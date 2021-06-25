052c710e0810c2784351ee5d097837e49aff2a37
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-06-25 16:14:37 -0300

    Finalizei a aula 01

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { useState } from 'react'
-import { View, Text, TextInput, Image } from 'react-native'
+import { View, Text, TextInput, Image, StatusBar } from 'react-native'
 import {styles} from './styles'
-
+import { ButtonIcon } from '../../components/ButtonIcon'
 import ilustrationImg from '../../assets/illustration.png'
 
 export function Sigin(){
 
-  const [text, setText] = useState("")
 
   return(
     <View style={styles.container}>
-     <Image source={ilustrationImg} 
-      style={styles.image}>
+
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        >
+        
+      </StatusBar>
+
+     <Image 
+      source={ilustrationImg} 
+      style={styles.image}
+      resizeMode="stretch"
+      >
       
       </Image>
 
-    <View styles={styles.content}>
+    <View style={styles.content}>
       <Text style={styles.title}>
         Organize {`\n`}
         suas jogatinas {`\n`}
@@ -27,6 +38,11 @@ export function Sigin(){
         Crie grupos para jogar seus games {`\n`}
         favoritos com seus amigos
       </Text>
+
+      <ButtonIcon 
+        title={"Entre com discord"}
+        activeOpacity={.7}
+      ></ButtonIcon>
     </View>
 
     </View>
